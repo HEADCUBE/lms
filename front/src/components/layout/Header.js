1e52701789e7e34c131a33d68bbad30763010488
HEADCUBE <omarkhalfi2010@gmail.com>
2024-01-16 12:31:17 +0100

    hana ghadi

---
+++ front/src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import Link from "next/link"
+export default function Header(){
+    return(
+    <header>
+    <div className="hidden md:flex items-center justify-between">
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link className="text-primary font-semibold text-2xl" href={'/'}>
+            Pizza napolitana
+          </Link>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/menu'}>Menu</Link>
+          <Link href={'/#about'}>About</Link>
+          <Link href={'/#contact'}>Contact</Link>
+        </nav>
+     </div>
+ 
+     </header> )
+}
